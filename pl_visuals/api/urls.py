62ce34a02e9f20514a3f5ff4a2b512a771ade996
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2023-06-27 17:11:43 +0200

    Updated urls

--- pl_visuals/api/urls.py
+++ pl_visuals/api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("club/", views.league_data_list_create_view),
-    path("club/<int:pk>/", views.league_data_detail_view),
-    path("match/", views.league_data_list_create_view),
-    path("match/<int:pk>/", views.league_data_detail_view),
+    path("club/", views.club_list_create_view),
+    path("club/<int:pk>/", views.club_detail_view),
+    path("match/", views.match_list_create_view),
+    path("match/<int:pk>/", views.match_detail_view),
     path("league/", views.league_data_list_create_view),
     path("league/<int:pk>/", views.league_data_detail_view),
     path("competition/", views.competition_data_list_create_view),
