e3a578fc7c9c9157c2f0e8495842351ea2b140d5
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2023-06-28 10:58:40 +0200

    New post data

--- api_client/club.py
+++ api_client/club.py
@@ -3,7 +3,7 @@ import csv
 
 endpoint = "http://localhost:8000/api/club/"
 
-data = {"club_name": "Manchester United", "club_id": 10260}
+data = {"club_name": "Arsenal", "club_id": 9825}
 
 get_response = requests.post(endpoint, json=data)
 print(get_response.json())
