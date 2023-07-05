64a1acca1b6792375ee09a4923d5c7f45f112752
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2023-07-05 17:12:23 +0200

    Updated database connection

--- pl_visuals/pl_visuals/settings.py
+++ pl_visuals/pl_visuals/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = "pl_visuals.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
